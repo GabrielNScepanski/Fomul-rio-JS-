718e42c0b698cd63cc83fd3d7cf8462751fc9a89
GabrielNScepanski <161836677+GabrielNScepanski@users.noreply.github.com>
2024-03-11 09:02:52 -0300

    Add files via upload

---
+++ valida_form.js
@@ -0,0 +1,35 @@
+const form=document.getElementById("#forn");
+const nameInput=document.querySelector("#name");
+const cpfinput=document.querySelector("#cpf");
+const emailinput=document.querySelector("#email");
+const passwordinput=document.querySelector("#passord");
+const jobslectinput=document.querySelector("#job");
+const messageTextaes=document.querySelector("#message");
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault
+
+    if (passwordinput, value===" ") ;
+    return;   
+        }
+    if (nameInput, value=== " "){
+    alert (Por favor preencha seu nome);
+    return;   
+        }
+     if (cpfinput, value=== " "){
+    alert (Por favor preencha seu cpf);
+    return valid ;   
+        }            any
+    if (mailinput, value=== " ") {
+    alert (Por favor preencha seu Email);
+    return valid ;   
+        }
+    if (jobslectinpu, value=== " ") {
+    alert (Por favor preencha sua escolha);
+    return;   
+        }
+    if (messageTextaes, value=== " "){
+    alert (Por favor preencha uma menssagem);
+    return;   
+        }
+   
